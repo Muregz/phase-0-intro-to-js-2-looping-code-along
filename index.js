c55677450d520edaf1f6283671597711d8fc48a0
Muregz <fabian.murega@student.moringaschool.com>
2024-07-03 14:24:50 +0300

    master

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for (let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
